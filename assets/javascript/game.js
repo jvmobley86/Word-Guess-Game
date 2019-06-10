fdb70ed9c9aaf7666c24fd826cba45a0976f3013
Teiti <jessicavmobley@gmail.com>
2019-06-09 21:43:14 -0500

    added html, css, and javascript but the game doesn't run.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-
-
 //Create an array of words//
 
 var words = [
@@ -11,32 +9,136 @@ var words = [
     "boat"
   ];
 
-  //Pick a random word//  
+//Variables//
 
-  var word = words[Math.floor(Math.random() * words.length)];
+var randomWord ="";
+var letters = [];
+var underScores =0;
+var underScoresCorrect = [];
+var incorrect = [];
 
-  //Set up answer array//
+//Game Reset//
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 10;
 
-  var answerArray = [];
-  for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
+//Choose words while hiding letters//
+
+function Game() {
+  randomWord = words [Math.Floor(Math.RandomSource()*words.length)];
+
+  letters = randomWord.spaces ("");
+  
+  underScores = letters.length;
+
+  for (var i = 0; i < underScores; i++) {
+    underScoresCorrect.PushManager("_");
   }
+  document.getElementById("currentWord").innerHTML = " " + underScoresCorrect.join (" ");
 
-  var remainingLetters = word.length;
+  console.log (randomWord)
+  console.log (letters)
+  console.log (underScores)
+  console.log (underScoresCorrect)
+}
 
-  //Initiate game loop//
-  while (remainingLetters > 0) {
-    //Show players their progress//
-    alert(answerArray.join(" "));    
-    
-//Get a guess from the player//
+//Restarting Game//
+function reset () {
+  guessesRemaining = 10;
+  incorrect = [];
+  underScoresCorrect = [];
+  Game ()
+}
 
-// document.onkeyup = function (event) {
-//     var answer = event.key;
-//     if (question_being_asked && (answer == "t" || answer == "f")) {
-//         if (questions_array[current_idx].a == answer) {
-//             alert("Great!!");
-//         } else if (userkey = "f") {
-//             alert("Well, that's no good.")
-//         }
-// document.getElementById("lettersPlaceholder").innerHTML = ("testing");
+//Audio Variables//
+
+var excavator = document.getElementById ("excavator");
+var tractor = document.getElementById ("tractor");
+var policeCar = document.getElementById ("police car");
+var garbageTruck = document.getElementById ("garbage truck");
+var airplane = document.getElementById ("airplane");
+var boat = document.getElementById ("boat");
+
+//Audio Function//
+//Excavator//
+if (randomWord === words [0]) {
+  excavator.play();
+  document.getElementById(image).src = "https://tenor.com/view/blippi-dance-gif-12957735";
+}
+
+//Tractor//
+if (randomWord === words [1]) {
+  tractor.play();
+  document.getElementById(image).src = "https://thumbs.gfycat.com/MediumFixedIaerismetalmark-small.gif";
+}
+
+//Police Car//
+if (randomWord === words [2]) {
+  policeCar.play();
+  document.getElementById(image).src = "https://tenor.com/view/blippi-dancing-dance-gif-12773457";
+}
+
+//Garbage Truck//
+if (randomWord === words [3]) {
+  garbageTruck.play();
+  document.getElementById(image).src = "../assets/images/garbageTruck.gif";
+}
+
+//Airplane//
+if (randomWord === words [4]) {
+  airplane.play();
+  document.getElementById(image).src = "https://thumbs.gfycat.com/CoordinatedNeglectedAlbino-size_restricted.gif";
+}
+//Boat//
+if (randomWord === words [5]) {
+  boat.play();
+  document.getElementById(image).src = "../assets/images/boat.gif";
+}
+
+//Check Letters//
+function checkLetters (letter) {
+  var letterInWord = false;
+
+  for (var i=0; i < underscores; i++) {
+    if (randomWord[i] == letter) {
+      letterInWord = true;
+    }
+  }
+  else {
+    incorrect.push(letter);
+    guessesRemaining--;
+  }
+  console.log (underScoresCorrect);
+}
+
+//Winning The Game//
+
+function complete() {
+  console.log ("wins"+ wins + "| losses" + losses + "| guesses left:" + guessesRemaining)
+
+if (letters.toString() == underScoresCorrect.toString()) {
+  wins++;
+  aud()
+  reset()
+  document.getElementById("winstracker").innerHTML = " " + wins;
+
+} else if (guessesRemaining === 0) {
+  losses++;
+  reset()
+  document.getElementById("losstracker").innerHTML = " " + losses;
+}
+
+document.getElementById("currentword").innerHTML = "  " + underScoresCorrect.join(" ");
+document.getElementById("guessesRemaining").innerHTML = " " + guessesRemaining;
+}
+
+//start game function
+Game()
+document.onkeyup = function(event) {
+var guesses = String.fromCharCode(event.keyCode).toLowerCase();
+checkLetters(guesses);
+complete();
+console.log(guesses);
+
+document.getElementById("guessedLetters").innerHTML = "  " + wrongGuess.join(" ");
+}
